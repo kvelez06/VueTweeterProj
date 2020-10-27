b6f0e4707e7f2972ec6179964036777435331594
Karl <heroware.06@hotmail.com>
2020-10-27 15:43:59 -0600

    the real 99th redo

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import VueRouter from "vue-router";
 import SignupPage from "../views/SignUp.vue";
 import FeedPage from "../views/Feed.vue";
 import AccountPage from "../views/Account.vue";
+import UserTweets from "../components/UserTweets.vue";
+import UserProfile from "../components/UserProfile.vue";
+import ProfilesPage from "../views/Profiles.vue";
 
 
 Vue.use(VueRouter);
@@ -22,8 +25,28 @@ const routes = [
     path: "/account",
     name: "account-page",
     component: AccountPage
+  },
+  {
+    path: "/",
+    name: "login-page",
+    component: LoginPage
+  },
+  {
+    path: "/tweets",
+    name: "user-tweets",
+    component: UserTweets
+  },
+  {
+    path: "/profile",
+    name: "user-profile",
+    component: UserProfile
+  }
+    path: "/profiles",
+    name: "profiles-page",
+    component: ProfilesPage
   }
 ];
+
 
 const router = new VueRouter({
   routes
